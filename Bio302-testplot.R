b714d1c79343714177fd5c8312441fa1671d6731
HannahSodal <hasod4069@uib.no>
2025-03-31 11:21:52 +0200

    Changes in plot and branch merge

--- Bio302-testplot.R
+++ Bio302-testplot.R
@@ -4,13 +4,14 @@ library(beeswarm)
 ## I am making a short plot of pamlmer pengins to see how to commit into github
 
 ### palmer penguins plot
-ggplot(data = penguins, aes(x= species, y = body_mass_g, colour = island, fill = island)) +
+ggplot(data = penguins, aes(x= species, 
+                            y = body_mass_g, 
+                            fill = island)) +
   geom_boxplot() +
   theme_bw() +
-  scale_colour_viridis_d() +
   scale_fill_viridis_d() +
   labs(title = "Body Mass by Species and Island",
        x = "Species",
        y = "Body Mass (g)",
-       colour = "Island") 
+       fill = "Island") 
   
